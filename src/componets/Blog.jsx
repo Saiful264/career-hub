35c606e710d6b0731d1ea32d4ebd47cb4f4f9dc0
Saiful264 <saifulislamx264@gmail.com>
2023-04-12 05:37:44 +0600

    statistics page chat been added

--- src/componets/Blog.jsx
+++ src/componets/Blog.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
 
 const Blog = () => {
-    return (
-        <>
-            
-        </>
-    );
+  return (
+    <div className="px-52 pt-10">
+      <h1 className="text-2xl font-medium text-slate-800">
+        When should you use context API?
+      </h1>
+      <p>Ans:</p>
+      <h1 className="text-2xl font-medium text-slate-800">
+        What is a custom hook?
+      </h1>
+      <p>Ans:</p>
+      <h1 className="text-2xl font-medium text-slate-800"> What is useRef?</h1>
+      <p>Ans:</p>
+      <h1 className="text-2xl font-medium text-slate-800">What is useMemo?</h1>
+      <p>Ans:</p>
+    </div>
+  );
 };
 
 export default Blog;
--- src/componets/Statistics.jsx
+++ src/componets/Statistics.jsx
@@ -1,9 +1,73 @@
 import React from 'react';
+import {
+    AreaChart,
+    Area,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip
+  } from "recharts";
+
+const data = [
+    {
+        "id": "1",
+        "name": "Assignment 1",
+        "mark": 54
+    },
+    {
+        "id": "2",
+        "name": "Assignment 2",
+        "mark": 59
+    },
+    {
+        "id": "2",
+        "name": "Assignment 3",
+        "mark": 59
+    },
+    {
+        "id": "3",
+        "name": "Assignment 4",
+        "mark": 60
+    },
+    {
+        "id": "4",
+        "name": "Assignment 5",
+        "mark": 56
+    },
+    {
+        "id": "5",
+        "name": "Assignment 6",
+        "mark": 56
+    },
+    {
+        "id": "6",
+        "name": "Assignment 7",
+        "mark": 60
+    },
+    {
+        "id": "7",
+        "name": "Assignment 8",
+        "mark": 58
+    }
+];
 
 const Statistics = () => {
     return (
-        <div>
-            <h1>This is statistics page</h1>
+        <div className='px-28 pt-12 flex flex-col items-center'>
+            <h1 className='text-3xl font-medium text-slate-800 pb-8'>This is my Assignment statistics until now.</h1>
+            <div>
+            <AreaChart
+              width={800}
+              height={400}
+              data={data}
+            >
+                <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+                <Area type="monotone" dataKey="mark" stroke="#8884d8" fill="#8884d8" />
+            </AreaChart>
+            </div>
         </div>
     );
 };
