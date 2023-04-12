e56d09c2b341c60c4f2048ae83086cfab0a60ec8
Saiful264 <saifulislamx264@gmail.com>
2023-04-12 11:30:32 +0600

    error page work down

--- src/componets/ErrorPage.jsx
+++ src/componets/ErrorPage.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-// import { Link, useRouteError } from 'react-router-dom'
+import { Link, useRouteError } from 'react-router-dom'
 
 const ErrorPage = () => {
-  // const { error, status } = useRouteError()
+  const { error, status } = useRouteError()
   return (
     <section className='flex items-center h-screen p-16 bg-gray-100 text-gray-900'>
       <div className='container flex flex-col items-center justify-center px-5 mx-auto my-8'>
@@ -33,17 +33,17 @@ const ErrorPage = () => {
         </svg>
         <div className='max-w-md text-center'>
           <h2 className='mb-8 font-extrabold text-9xl text-gray-600'>
-            {/* <span className='sr-only'>Error</span> {status || 404} */}
+            <span className='sr-only'>Error</span> {status || 404}
           </h2>
           <p className='text-2xl font-semibold md:text-3xl mb-8'>
-            {/* {error?.message} */}
+            {error?.message}
           </p>
-          {/* <Link
+          <Link
             to='/'
             className='px-8 py-3 font-semibold rounded bg-cyan-200 text-gray-900'
           >
             Back to homepage
-          </Link> */}
+          </Link>
         </div>
       </div>
     </section>
--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="flex justify-around items-center py-12 bg-slate-100">
+    <div className="flex justify-between items-center px-28 py-12 bg-slate-100">
         <Link className="font-bold text-3xl" to="/">CareerHive</Link>
         <div className="">
             <Link className="pl-8 bg-gradient-to-r from-green-400 to-blue-500 bg-clip-text text-transparent font-semibold text-sm" to="/statistics">Statistics</Link>
